725f63f6597aee6829bd82b2d4dd3f117ccd2134
James <jayninja@fazey.org>
2017-03-28 00:38:27 -0500

    added some stuff to tell creation time

---
+++ crtime/crtime.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+require 'open3'
+
+file = ARGV[0]
+
+## I know, but how the hell else am i supposed to do it?
+partition = `df -T #{file} | grep dev `
+
+# this is lame too, in order to get the debugfs header to not show
+# you have to use open3. Theres no amount of redirecting to do it.
+file_inode = File.stat(file).ino
+stdout, stderr, status = Open3.capture3("debugfs -R 'stat <#{file_inode}>' #{partition}")
+
+# split it by new lines 
+result_split = stdout.split("\n")
+
+# go through each line and find the one with crtime
+result_split.each { |local_result|
+if ( local_result =~ /crtime(.*)/ )
+  puts local_result
+  end
+}
---
+++ crtime/rpm/crtime.spec
@@ -0,0 +1,29 @@
+Name: crtime	
+Version: 1.0	
+Release: 1
+Summary: A script to tell you creation time of a file
+
+Group: System/Utilities	
+License: GPL
+URL: http://www.fazey.org
+Source0: crtime.tar.gz
+
+Requires: ruby	
+
+%description
+A script to tell you creation time of a file
+
+%prep
+%setup -q -n crtime
+
+%install
+mkdir -p $RPM_BUILD_ROOT/usr/bin
+cp crtime.rb $RPM_BUILD_ROOT/usr/bin/crtime
+
+
+%files
+/usr/bin/crtime
+
+
+%changelog
+
